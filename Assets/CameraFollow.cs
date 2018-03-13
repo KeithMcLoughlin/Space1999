b7b8bf8fed41bc9d33f852d4c2bd84f8b60d3619
Keith <kmcl1321@gmail.com>
2018-03-13 11:00:53 +0000

    added camera follow

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets
+{
+    public class CameraFollow : MonoBehaviour
+    {
+        public Boid target;
+
+        void Update()
+        {
+            Vector3.Lerp(transform.position, target.transform.position, Time.deltaTime);
+        }
+    }
+}
